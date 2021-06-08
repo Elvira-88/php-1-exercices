4f031da18c09a213c82c667248b2dd9947fa06cd
Elvira-88 <elvipt88@gmail.com>
2021-06-08 08:47:39 +0200

    Inicio ejs visibles en el servidor PHP

---
+++ public/enlaces.php
@@ -0,0 +1,3 @@
+<a href="http://localhost:8080">index</a>
+<a href="http://localhost:8080/employees.php">employees</a>
+<a href="http://localhost:8080/employees_json.php">employees_json</a>
--- src/ejercicios.php
+++ src/ejercicios.php
@@ -29,3 +29,27 @@ function averageAge(array $people): int {
     $totalAge = $sum / count($people);
     return $totalAge;
 }
+
+function reverseString (string $string): string { // $tring = Hola qué tal
+    $chars = mb_str_split($string);
+    
+    return implode('', array_reverse($chars));
+}
+
+function reverseWords (string $string): string {
+    $words = explode(' ', $string);
+
+    return implode(' ', array_reverse($words));
+}
+
+function reverseCharactersInWords (string $string): string {
+    $words = explode(' ', $string);
+
+    $turnWords = [];
+
+    foreach($words as $word) {
+        $turnWords[] = reverseString($word);
+    }
+
+    return implode(' ', $turnWords);
+}
