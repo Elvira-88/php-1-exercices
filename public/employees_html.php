4e4ff0d49639f3f67ceb29501dadbb0765e58066
elvira <elvipt88@gmail.com>
2021-06-09 13:58:28 +0200

    Formulario en BBDD employees de PHPMyAdmin

--- public/employees_html.php
+++ public/employees_html.php
@@ -15,13 +15,37 @@
         <tbody>
             <?php foreach($people as $person): ?>
                 <tr>
+                    <td><?= $person['id'] ?></td>
                     <td><?= $person['name'] ?></td>
-                    <td><?= $person['email'] ?></td>
+                    <td><a href="/employees.php?id=<?= $person['id']; ?>"><?= $person['email'] ?></a></td>
                     <td><?= $person['city'] ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
+
+    <?php if(isset($_GET['message'])) :?>
+        <p><?= $_GET['message']; ?></p>
+    <?php endif; ?>
+
+    <hr/>
+
+    <form method="POST" action="/employees_add.php" enctype="multipart/form-data">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" name="name" required/>
+        <br/>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required/>
+        <br/>
+        <label for="age">Edad</label>
+        <input type="number" id="age" name="age" required/>
+        <br/>
+        <label for="city">Ciudad</label>
+        <input type="text" id="city" name="city" />
+        <hr/>
+        <input type="submit" value="Enviar"/>
+    </form>
+
     <?php require 'partial/footer.php'; ?>
 </body>
 
